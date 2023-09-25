d13cb94458febaaafa724834a2bf1bd4230a07d4
Francisco Ferreira <114003387+ferreirafrancisco0@users.noreply.github.com>
2023-09-25 11:03:09 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+ 
+    public static void main(String [] args){
+        Aluno aluno1 = new Aluno("Chico", 123456789,10, 9 );
+        aluno1.calcularMedia();
+        aluno1.imprimirInformacoes();
+    }
+}
